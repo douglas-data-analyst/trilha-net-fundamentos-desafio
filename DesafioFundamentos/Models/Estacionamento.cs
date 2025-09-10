c9d6e079481ada269dfec37874fe58b1d701f20b
douglas-data-analyst <dr970788@gmail.com>
2025-09-10 14:12:52 -0300

    Realizado a implementação da solução para remover a placa cadastrada

--- DesafioFundamentos/Models/Estacionamento.cs
+++ DesafioFundamentos/Models/Estacionamento.cs
@@ -17,8 +17,10 @@ namespace DesafioFundamentos.Models
             // TODO: Pedir para o usuário digitar uma placa (ReadLine) e adicionar na lista "veiculos"
             // R:       Realizado a validação da placa com o método criado na classe ValidationHelper, 
             //          também adicionei uma verificação para não cadastrar placas repetidas.
+
             string placa = ValidationHelper.LerPlacaComValidacao("\nDigite a placa do veículo para estacionar: ");
                 // Verificação se a placa já está cadastrada
+
                 if (veiculos.Any(x => x.ToUpper() == placa.ToUpper()))
                 {
                     Console.WriteLine("\nEste veículo já está estacionado!\n");
@@ -30,27 +32,33 @@ namespace DesafioFundamentos.Models
 
         public void RemoverVeiculo()
         {
-            Console.WriteLine("Digite a placa do veículo para remover:");
-
             // Pedir para o usuário digitar a placa e armazenar na variável placa
-            // *IMPLEMENTE AQUI*
-            string placa = "";
+            // R:       Realizado a validação da placa com o método criado na classe ValidationHelper.
+
+            string placa = ValidationHelper.LerPlacaComValidacao("\nDigite a placa do veículo para remover: ");
 
             // Verifica se o veículo existe
+
             if (veiculos.Any(x => x.ToUpper() == placa.ToUpper()))
             {
-                Console.WriteLine("Digite a quantidade de horas que o veículo permaneceu estacionado:");
-
                 // TODO: Pedir para o usuário digitar a quantidade de horas que o veículo permaneceu estacionado,
                 // TODO: Realizar o seguinte cálculo: "precoInicial + precoPorHora * horas" para a variável valorTotal                
-                // *IMPLEMENTE AQUI*
+                // Realizado a validação de horas com o método criado na classe ValidationHelper, realização do calculo do  
+                // valor a ser pago.
+
                 int horas = 0;
-                decimal valorTotal = 0; 
+                decimal valorTotal = 0;
+
+                int horas = ValidationHelper.LerInteiroComValidacao("\nDigite a quantidade de horas que o veículo permaneceu estacionado: ");
+
+                decimal valorTotal = precoInicial + (precoPorHora * horas);
 
                 // TODO: Remover a placa digitada da lista de veículos
-                // *IMPLEMENTE AQUI*
+                // Remove a placa digitada da lista de veículos
 
-                Console.WriteLine($"O veículo {placa} foi removido e o preço total foi de: R$ {valorTotal}");
+                veiculos.Remove(placa);
+
+                Console.WriteLine($"O veículo {placa} foi removido e o preço total foi de: R$ {valorTotal:F2}");
             }
             else
             {
